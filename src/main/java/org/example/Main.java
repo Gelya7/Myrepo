bce0e3f0061d0d0c2a74bde5d9e230b3d91e1ac1
Bohdan Romanchenko <Brom@Bohdans-MacBook-Pro.local>
2024-12-09 19:33:54 +0200

    set up

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println("Number is " + num);
+        num = 55;
+        Sys
+    }
+}
